488d9a06a0cd8dad57166143b6c871374aacfa58
Peter Mackay <project2501@gmail.com>
2019-08-22 16:10:35 -0600

    added header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Box, Grid, Container, Typography} from "@material-ui/core";
+import { styled, makeStyles } from '@material-ui/styles';
+
+import NavBar from './Components/Navbar'
+import Portfolio from './Components/Portfolio'
+
 
 function App() {
+  const AppBackground = styled(Box)({
+    background: 'repeating-linear-gradient(-45deg, #1a1a1a, #1a1a1a 6px, #222 6px, #222 12px)',
+    height:'100vh'
+  })
+
+  const useStyles = makeStyles(theme => ({
+    titleHeader: {
+      color: '#ff0080',
+      textAlign: 'center',
+      margin: '25px',
+      fontFamily: "objektiv-mk1, sans-serif",
+      fontWeight: '900',
+      fontStyle: 'italic',
+    },
+    titleSubHeader:{
+      color: '#555555',
+      textShadow:
+        '-1px -1px 0 #666666, 1px 1px 0 #ff0080',
+      textAlign: 'center',
+      margin: '5px',
+      fontFamily: "objektiv-mk1, sans-serif",
+      fontWeight: '700',
+      fontStyle: 'italic',
+      "&:hover":{
+        position:'relative',
+        top:'-3px',
+        left:'-3px',
+        textShadow:
+        '-1px -1px 0 #777777, 1px 1px 0 #ff0080, 2px 2px 0 #ff0080, 3px 3px 0 #ff0080, 4px 4px 0 #ff0080',
+      },
+    },
+    titleBox: {
+      flexGrow: 1,
+    },
+  }))
+
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppBackground>
+      <NavBar />
+      <Container >
+        <Grid container className={classes.titleBox}>
+          <Grid item xs={12}>
+            <Typography variant="h1" className={classes.titleHeader}>
+              PETER MACKAY
+            </Typography>
+          </Grid>
+          <Grid item xs={6}>
+            <Typography variant="h4" className={classes.titleSubHeader}>
+              peter.mackay.1187@gmail.com
+            </Typography>
+          </Grid>
+          <Grid item xs={6}>
+            <Typography variant="h4" className={classes.titleSubHeader}>
+              github.com/ninjavitis
+            </Typography>
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={3} xs={12}>
+        </Grid>
+      </Container>
+    </AppBackground>
   );
 }
 
---
+++ src/Components/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { AppBar, Toolbar } from "@material-ui/core";
+import { styled } from '@material-ui/styles';
+
+
+const  Navbar = () => {
+  const TopBar = styled(AppBar)({
+    background: '#1a1a1a',
+  })
+
+  return (
+    <TopBar position="static">
+      <Toolbar>
+
+      </Toolbar>
+    </TopBar>
+  );
+}
+
+export default Navbar;

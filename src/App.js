a5796547902909d0e7c835b91a3743109f6fd094
Peter Mackay <project2501@gmail.com>
2019-08-22 16:54:58 -0600

    summary box

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Box, Grid, Container, Typography} from "@material-ui/core";
+import { Box, Grid, Container, Typography, Paper} from "@material-ui/core";
 import { styled, makeStyles } from '@material-ui/styles';
 
 import NavBar from './Components/Navbar'
@@ -9,15 +9,18 @@ import Portfolio from './Components/Portfolio'
 
 function App() {
   const AppBackground = styled(Box)({
-    background: 'repeating-linear-gradient(-45deg, #1a1a1a, #1a1a1a 6px, #222 6px, #222 12px)',
+    background: 'repeating-linear-gradient(-45deg, #111111, #111111 6px, #1a1a1a 6px, #1a1a1a 12px)',
     height:'100vh'
   })
 
   const useStyles = makeStyles(theme => ({
+    headerBox:{
+      marginBottom:'50px'
+    },
     titleHeader: {
       color: '#ff0080',
       textAlign: 'center',
-      margin: '25px, 0px, 10px, 0px',
+      marginTop: '25px',
       fontFamily: "objektiv-mk1, sans-serif",
       fontWeight: '900',
       fontStyle: 'italic',
@@ -42,6 +45,16 @@ function App() {
     titleBox: {
       flexGrow: 1,
     },
+    summary:{
+      background:'#252525', 
+      padding: '25px',
+    },
+    summaryText:{
+      color: '#999999',
+      fontFamily: "objektiv-mk1, sans-serif",
+      fontWeight: '400',
+      fontStyle: 'normal'
+    }
   }))
 
   const classes = useStyles();
@@ -50,8 +63,8 @@ function App() {
     <AppBackground>
       <NavBar />
       <Container >
-        <Grid container className={classes.titleBox}>
-          <Grid item xs={12}>
+        <Grid container className={classes.headerBox}>
+          <Grid item xs={12} className={classes.titleHeader}>
             <Typography variant="h1" className={classes.titleHeader}>
               PETER MACKAY
             </Typography>
@@ -67,7 +80,14 @@ function App() {
             </Typography>
           </Grid>
         </Grid>
-
+        <Paper className={classes.summary}>
+          <Typography paragraph className={classes.summaryText}>
+            I'm a highly motivated and passionate developer with over 7 years of multidisciplinary PC and console game development experience in QA, Project Management and System Design roles. I also have 5+ years of event production and travel logistics experience as well a background in datacenter operations and system admin. Recently graduated from the University of Utah full-stack certificate program with a focus on Javascript, React, Ruby and Rails.
+          </Typography>
+          <Typography paragraph className={classes.summaryText}>
+            In my spare time I develop an agent based economic simulator in c++, a Android mobile clicker game in Unreal Engine 4 and a demo e-commerce platform in React.js. 
+          </Typography>
+        </Paper>
         <Grid container spacing={3} xs={12}>
         </Grid>
       </Container>

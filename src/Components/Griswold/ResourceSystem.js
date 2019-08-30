a1a890b2096e5a40c550a6cf56216961a7593f62
Peter Mackay <project2501@gmail.com>
2019-08-30 11:05:40 -0600

    new screenshots

--- src/Components/Griswold/ResourceSystem.js
+++ src/Components/Griswold/ResourceSystem.js
@@ -22,7 +22,7 @@ const Griswold = () => {
         <DetailCard 
           header="Resource System"
           subheader="Unreal Enginge 4 Blueprint Script"
-          images={["/images/griswold/SHIP_scripted_functions.png"]}
+          images={["/images/griswold/SCRIPTING_resourceManagement.png"]}
           videoUrl="https://www.youtube.com/embed/YXxtB8JQDI0"
           body={["Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."]}
         />

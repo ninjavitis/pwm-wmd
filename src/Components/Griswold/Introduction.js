2479f34b64d53b6cbd7fe54f8d79346343d426e9
Peter Mackay <project2501@gmail.com>
2019-09-07 18:05:53 +0900

    updated project griswold introduction message

--- src/Components/Griswold/Introduction.js
+++ src/Components/Griswold/Introduction.js
@@ -30,7 +30,7 @@ const Griswold = () => {
         <Grid item container justify="center">
         <Paper className={classes.foregroundPaper}>
         <Typography paragraph>
-          Project Griswold is a testbed + sandbox for an agent based economy simulation I have written in c++ for Unreal Engine 4.
+          Project Griswold is a testbed + sandbox for an agent based economy simulation I have written in c++ for Unreal Engine 4.  All the code is authored by me and uses no UE4 starter project or example code or scripts.
         </Typography>
         <Typography paragraph>
           Each of the pages in this section discuss an individual feature of the project with script screenshots and video showing the feature in action.  Finally, you can view a selection of header files by following the github link to the left.  

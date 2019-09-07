6d97016bd83aa53ceb2ff979310c5a0953192fe0
Peter Mackay <project2501@gmail.com>
2019-09-07 18:13:31 +0900

    added gearbox/relic attribution

--- src/Components/Griswold/Introduction.js
+++ src/Components/Griswold/Introduction.js
@@ -33,10 +33,10 @@ const Griswold = () => {
           Project Griswold is a testbed + sandbox for an agent based economy simulation I have written in c++ for Unreal Engine 4.  All the code is authored by me and uses no UE4 starter project or example code or scripts.
         </Typography>
         <Typography paragraph>
-          Each of the pages in this section discuss an individual feature of the project with script screenshots and video showing the feature in action.  Finally, you can view a selection of header files by following the github link to the left.  
+          Each of the pages in this section discuss an individual feature of the project with script screenshots and video showing the feature in action.  Finally, you can view a selection of header files by following the github link to the left.            I'm happy to showcase the full project source in person, and a packaged executable is available upon request.  
         </Typography>
         <Typography>
-          I'm happy to showcase the full project source in person, and a packaged executable is available upon request.
+          Some art has been borrowed from the Gearbox/Relic classic 'Homeworld 2' and is not for release.
         </Typography>
       </Paper>
         </Grid>
